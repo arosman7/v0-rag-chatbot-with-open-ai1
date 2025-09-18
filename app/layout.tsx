947ff779c0cda57c906b46cc63d827888363def4
arosman7 <150029926+arosman7@users.noreply.github.com>
2025-09-18 14:14:52 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,9 +5,9 @@ import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
+  title: 'Ветеринария',
   description: 'Created with v0',
-  generator: 'v0.app',
+  generator: Ветеринария',
 }
 
 export default function RootLayout({
